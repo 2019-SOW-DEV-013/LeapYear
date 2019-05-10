bd856c59dd827875e54182be46539fe792bbaa87
2019-SOW-DEV-013 2019-SOW-DEV-013 <sowdev013@gmail.com>
2019-05-10 08:27:45 +0200

    style: Format index file

--- src/index.js
+++ src/index.js
@@ -1,25 +1,25 @@
 var Year = require('./year');
 
-var MESSAGE={
-LEAP_YEAR: "Leap year",
-NON_LEAP_YEAR: "Not a leap year",
-INVALID_YEAR: "Invalid Year. Please Enter a valid year from 1582 as Gregorian Calendar is adopted from 1582"
+var MESSAGE = {
+    LEAP_YEAR: "Leap year",
+    NON_LEAP_YEAR: "Not a leap year",
+    INVALID_YEAR: "Invalid Year. Please Enter a valid year from 1582 as Gregorian Calendar is adopted from 1582"
 };
 
 document.getElementById("check").onclick = function () {
 
     var year = new Year(Number(document.getElementById("year").value));
-    if (year.isValidYear()){
-        if(year.isLeapYear()){
+    if (year.isValidYear()) {
+        if (year.isLeapYear()) {
             displayMessage(MESSAGE.LEAP_YEAR);
-        }else{
+        } else {
             displayMessage(MESSAGE.NON_LEAP_YEAR);
         }
-    }else{
+    } else {
         displayMessage(MESSAGE.INVALID_YEAR);
     }
 }
 
-function displayMessage(message){
-    document.getElementById("message").innerHTML =message;
+function displayMessage(message) {
+    document.getElementById("message").innerHTML = message;
 }
